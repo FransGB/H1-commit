096ff69553a3d9828148b7757db7070c3d3e8e1d
Frans Gabriel <148835240+FransGB@users.noreply.github.com>
2025-01-03 12:24:53 +0700

    Add files via upload

---
+++ H3/H3.py
@@ -0,0 +1,5 @@
+num1=int(input("Masukkan Angka Pertama Yang Mau Dijumlahkan :"))
+num2=int(input("Masukkan Angka Kedua Yang Mau Dijumlahkan :"))
+jumlah=(num1+num2)
+
+print("Hasil Dari Penjumlahan",num1,"Dan",num2,"Adalah :",jumlah)
