4dbeeaeb57545bfed315f746b9fd452fd3e919e2
Frans Gabriel <148835240+FransGB@users.noreply.github.com>
2025-01-04 17:42:45 +0700

    Add files via upload

---
+++ H4/List.py
@@ -0,0 +1,6 @@
+waifu=["Kohane","Yukino","Nino","An Shiraishi","Danielle"]
+
+print("Waifu Favorit Saya Adalah : {}".format(waifu[0]))
+print("Jumlah Waifu Saya Ada {} Orang, Yaitu :".format(len(waifu)))
+for wife in waifu:
+    print (wife)
