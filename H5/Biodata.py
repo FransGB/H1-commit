f42863fc71f8d6de81a0f0bb8e4d6f034d4d1add
Frans Gabriel <148835240+FransGB@users.noreply.github.com>
2025-01-12 21:18:02 +0700

    Biodata Python

--- H5/Biodata.py
+++ H5/Biodata.py
@@ -1,4 +1,4 @@
-print("Nama Saya Adalah Frans Gabriel")
+print("Nama Saya Adalah Frans Gabriel!")
 print("Saya Kuliah Di Universitas Sumatera Utara")
 print("Saya Kuliah Di Jurusan Teknik Informatika")
 print("Saya Berusia 20 Tahun")
